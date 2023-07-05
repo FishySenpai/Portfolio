0a5f044b4c95b796ca5f252468bf0ec074926d83
FishySenpai <akanomanbasit@gmail.com>
2023-07-05 16:20:47 +0500

    designed projects interface

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="relative ">
-      <div className="bg-slate-900 text-white p-3 font-semibold text-md">
+      <div className=" bg-gradient-to-r from-slate-600 from-10% via-slate-700 via-30% to-slate-900 to-90% text-white p-3 font-semibold text-md ">
         <div className="flex flex-row space-x-12 justify-center ">
           <div>
             <Link to="/">Home</Link>
