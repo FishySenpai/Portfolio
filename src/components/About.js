466d92658b62e622a0ab5635c3e0f7df4ae7c7f7
FishySenpai <akanomanbasit@gmail.com>
2023-11-11 17:26:44 +0500

    added skills and styled home page

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,75 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div className=" text-white">
       <div className="text-[50px] ml-96 pl-96 mt-96">ABOUT ME</div>
-      <div className="text-xl ml-96 mr-96 text-gray-200 ">
-        I specialize in crafting robust and dynamic web applications using
-        cutting-edge technologies. My toolkit includes:
-      </div>
-      <div className="flex flex-col ml-96 pl-5 mt-10 text-xl space-y-3 ">
-        <div className="px-3 py-2 bg-slate-500 rounded w-fit">Html</div>
-        <div className="px-3 py-2 bg-slate-500 rounded w-fit">Css</div>
-        <div className="px-3 py-2 bg-slate-500 rounded w-fit">TailwindCss</div>
-        <div className="px-3 py-2 bg-slate-500 rounded w-fit">Javascript</div>
-        <div className="px-3 py-2 bg-slate-500 rounded w-fit">Reactjs</div>
-        <div className="px-3 py-2 bg-slate-500 rounded w-fit">Firebase</div>
-        <div className="px-3 py-2 bg-slate-500 rounded w-fit">MySql</div>
+
+      <div className="flex flex-row mt-10">
+        <div className="text-xl text-gray-300 w-[600px] ml-96">
+          <p>
+            As a dedicated <strong>Full Stack Developer</strong>, I specialize
+            in creating robust and user-friendly websites and web applications
+            by seamlessly integrating both frontend and backend technologies. My
+            expertise extends from crafting engaging user interfaces to
+            architecting and optimizing scalable server-side solutions. The
+            <strong> Projects</strong> section showcases some of my notable
+            achievements, providing a glimpse into the comprehensive skill set I
+            bring to every project.
+          </p>
+          <br />
+          <p>
+            Beyond the code, I am committed to fostering knowledge-sharing
+            within the dynamic developer community. Connect with me on
+            <a href="#" className="underline text-purple-300">
+              {" "}
+              LinkedIn
+            </a>
+            , where I consistently share content spanning a wide array of topics
+            related to <strong>Full Stack Development</strong> and Programming.
+          </p>
+          <br />
+          <p>
+            Actively seeking new opportunities, I am enthusiastic about roles
+            that allow me to contribute holistically, continuously learn, and
+            foster personal and professional growth. If you have an exciting
+            opportunity that aligns with my skills and experiences, don't
+            hesitate to <strong>contact</strong> me. Let's collaborate and
+            create something extraordinary together!
+          </p>
+        </div>
+        <div className="flex flex-col ml-40 text-xl space-y-12 ">
+          <div className="text-2xl font-bold">Skills</div>
+          <div className="flex flex-row space-x-5">
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">HTML</div>
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">CSS</div>
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">
+              TailwindCss
+            </div>
+
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">
+              Bootstrap
+            </div>
+          </div>
+
+          <div className="flex flex-row space-x-5">
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">
+              JavaScript
+            </div>
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">Reactjs</div>
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">Firebase</div>
+          </div>
+          <div className="flex flex-row space-x-5">
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">MySql</div>
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">
+              Postgresql
+            </div>
+            <div className="px-3 py-2 bg-slate-500 rounded w-fit">Git</div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default About
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  const [isScrolledToBottom, setIsScrolledToBottom] = useState(false);
+
+  const handleScroll = () => {
+    const scrollThreshold =
+      document.documentElement.scrollHeight - window.innerHeight;
+    const isAtBottom = window.scrollY >= scrollThreshold;
+
+    setIsScrolledToBottom(isAtBottom);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div className="flex items-center justify-center">
       <div>
-        <div className="text-[50px] ml-96 pl-72 mt-96">HELLO, I'M FISHY SENPAI</div>
+        <div className="text-[50px] ml-96 pl-72 mt-96">
+          HELLO, I'M FISHY SENPAI
+        </div>
         <div className="text-xl ml-96 mr-96 text-gray-200 ">
           A passionate and results-driven web developer with a focus on creating
           immersive and user-friendly digital experiences. Whether you're a
@@ -12,6 +31,25 @@ const Home = () => {
           seeking to enhance your web applications, I'm here to turn your ideas
           into reality.
         </div>
+      </div>
+      <div className="fixed bottom-10 right-20 bg-white rounded-full px-4 py-3">
+        {isScrolledToBottom ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="30px"
+            viewBox="0 0 384 512"
+          >
+            <path d="M214.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 109.3V480c0 17.7 14.3 32 32 32s32-14.3 32-32V109.3l73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-128-128z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="30"
+            viewBox="0 0 384 512"
+          >
+            <path d="M169.4 502.6c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 402.7 224 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 370.7L86.6 329.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128z" />
+          </svg>
+        )}
       </div>
     </div>
   );
