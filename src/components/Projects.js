8d463c0d54ef82d96db33b0a5e72d9c580d5c2fb
FishySenpai <akanomanbasit@gmail.com>
2024-09-09 22:29:47 +0500

    updated projects bg

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -71,52 +71,75 @@ const Projects = () => {
   };
 
   return (
-    <div id="projects">
-      <div className="pt-16 ml-96 pl-22 mt-36 pb-10 text-[50px]">Projects</div>
+    <div id="projects" className="flex flex-col">
+      <div className="pt-16  mx-auto mt-36 pb-5 text-[50px] text-gray-100">
+        Projects
+      </div>
+      <div className="mx-auto pl-2 pb-8 max-w-[1100px]">
+        From Web Components and UI/UX animations to React.JS, Redux, Vue.JS, and
+        Node.JS. Check out my latest web software development portfolio
+        projects. From Web Components and UI/UX animations to React.JS, Redux,
+        Vue.JS, and Node.JS. Check out my latest web software development
+        portfolio projects.
+      </div>
       <div className="mx-auto max-w-[1100px]">
         <div className=" grid grid-cols-2 justify-center">
           {projects.map((project, index) => (
             <div>
-              <div className="relative group mb-10">
+              <div className="relative group mb-10 mr-10">
                 <div
-                  className="group w-[500px] h-[330px] rounded-lg overflow-hidden"
+                  className="group w-[500px] h-[330px] rounded-sm overflow-hidden"
                   onMouseOver={() => handleMouseOver(index)}
                   onMouseOut={() => handleMouseOut(index)}
                   onClick={() => handleProjectClick(project)}
                 >
-                  <a href="https://attirenova.netlify.app/" target="_blank">
+                  <a
+                    href="https://attirenova.netlify.app/"
+                    target="_blank"
+                    className={`w-[500px] h-[330px] relative  `}
+                  >
                     <img
-                      className={`image ${
-                        hoveredStates[index]
-                          ? "fade-in w-full h-full rounded-lg "
-                          : "w-full h-full rounded-lg "
+                      className={`w-full h-full rounded-sm  ${
+                        hoveredStates[index] ? "" : ""
                       }`}
-                      src={
-                        hoveredStates[index]
-                          ? project?.images[0]
-                          : project.imageUrl
-                      }
+                      src={project.imageUrl}
                       alt="Image description"
                     />
+                    <div
+                      className={`absolute inset-0 z-10 bg-neutral-900  duration-300 ${
+                        hoveredStates[index]
+                          ? "bg-opacity-90 "
+                          : "bg-opacity-0 "
+                      }`}
+                    >
+                      <img
+                        className={`w-full h-full z-0 rounded-sm overflow-hidden   ${
+                          hoveredStates[index]
+                            ? "absolute top-0 duration-300"
+                            : "absolute -top-[330px] duration-300 "
+                        }`}
+                        src={project?.images[0]}
+                        alt="Image description"
+                      />
+                    </div>
                   </a>
                   <button
                     onClick={() => setShowModal(true)}
-                    className="hidden group-hover:block cursor-pointer absolute top-0 left-0 right-0 bottom-0 bg-gray-800 bg-opacity-75 rounded-lg w-[500px]"
+                    className="hidden group-hover:block z-20 cursor-pointer absolute top-0 left-0 right-0 bottom-0 bg-neutral-900 bg-opacity-50  rounded-sm w-[500px]"
                   >
                     <div className="flex flex-col items-center justify-center h-full">
                       <ul className="flex flex-row space-x-5 font-bold text-[16px] text-slate-200 tag-list pt-28 pb-24">
                         {project.technologies.map((tech, index) => (
                           <li key={index} className="tag-animation">
-                            <div className="px-4 py-2 bg-slate-800 rounded shadow-md shadow-slate-950 w-fit">
+                            <div className="px-4 py-2 bg-[#444444] rounded shadow-md shadow-[#212121] w-fit">
                               {tech}
                               {console.log(tech)}
                             </div>
                           </li>
                         ))}
                       </ul>
-
                       <div className="details-animation font-bold text-[16px] text-slate-200 ">
-                        <div className="px-4 py-2 bg-slate-800 rounded shadow-md shadow-slate-950 w-fit">
+                        <div className="px-4 py-2 bg-[#444444] rounded shadow-md shadow-[#212121] w-fit">
                           details..
                         </div>
                       </div>
